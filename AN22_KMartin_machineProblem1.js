974d3d3beb7415ac6e6944314fadacea54c1d086
leimartin <leimartin1603@gmail,com>
2023-06-02 08:26:15 +0800

    Updating content

--- AN22_KMartin_machineProblem1.js
+++ AN22_KMartin_machineProblem1.js
@@ -1,13 +1,13 @@
 // ITCS227 Source Code Template for 2T AY 2022-2023
 /*
-	Program: 	Computation of Grades using Function
+	Program: 	  Computation of Grades using Function
 	Programmer: Katryna Lei V. Martin
-	Section: 	BCS22
+	Section: 	  AN22
 	Start Date: June 02, 2023
 	End Date: 	June 02, 2023
 */
 
-function calculateAverage(grades) {
+function calculateStudentAverage(grades) {
   return grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
 }
 
@@ -18,7 +18,7 @@ function computeLetterGrade(grade) {
     return 'B';
   } else if (grade >= 70 && grade < 80) {
     return 'C';
-  } else if (grade >= 60 grade < 70) {
+  } else if (grade >= 60 && grade < 70) {
     return 'D';
   } else {
     return 'F';
@@ -26,7 +26,7 @@ function computeLetterGrade(grade) {
 }
 
 function calculateStudentGrades() {
-  const resultsTable = document.getElementById('resultsTable');
+  const studentResultsTable = document.getElementById('studentResultsTable');
   
   for (let i = 1; i <= 5; i++) {
     const name = prompt(`Enter student ${i} name:`);
@@ -36,16 +36,18 @@ function calculateStudentGrades() {
       const enablingGrade = parseFloat(prompt(`Enter Enabling Assessment ${j} for ${name}:`));
       enablingGrades.push(enablingGrade);
     }
-    const classParticipation = calculateAverage(enablingGrades);
+    const classParticipation = calculateStudentAverage(enablingGrades);
 
     const summativeGrades = [];
     for (let k = 1; k <= 3; k++) {
       const summativeGrade = parseFloat(prompt(`Enter Summative Assessment ${k} for ${name}:`));
       summativeGrades.push(summativeGrade);
     }
-    const summativeGrade = calculateAverage(summativeGrades);
+    const summativeGrade = calculateStudentAverage(summativeGrades);
 
-    const overallGrade = (classParticipation + summativeGrade) / 2;
+    const finalExamGrade = parseFloat(prompt(`Enter Final Examination grade for ${name}:`));
+
+    const overallGrade = (classParticipation + summativeGrade + finalExamGrade) / 3;
     const letterGrade = computeLetterGrade(overallGrade);
 
     const row = document.createElement('tr');
@@ -53,6 +55,7 @@ function calculateStudentGrades() {
       <td>${name}</td>
       <td>${classParticipation.toFixed(2)}</td>
       <td>${summativeGrade.toFixed(2)}</td>
+      <td>${finalExamGrade.toFixed(2)}</td>
       <td>${overallGrade.toFixed(2)}</td>
       <td>${letterGrade}</td>
     `;
